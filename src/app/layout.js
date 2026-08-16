5f1c49f655d7feb6af3cc4f4244d47639e2a9c28
Vinit Kumar <vinitkumar8092@gmail.com>
2026-08-16 21:19:17 +0530

    Added new font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import "aos/dist/aos.css";
 import AosInitializer from "./components/AosInitializer";
 import FloatingActions from "./components/FloatingActions";
 
-const inter = Inter({
+const poppins = Poppins({
   subsets: ["latin"],
-  variable: "--font-inter",
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-poppins",
   display: "swap",
 });
 
@@ -25,14 +26,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${inter.variable} antialiased`} suppressHydrationWarning>
+    <html lang="en" className={`${poppins.variable} font-sans antialiased`} suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" href="/vidimethlogo1.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/vidimethlogo1.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body className="font-sans">
         <AosInitializer />
         {children}
         <FloatingActions />
